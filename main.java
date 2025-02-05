802ee182f9e101f93cd5826b09ecdbaed95aa446
Caroline <70326049+Caroline1609@users.noreply.github.com>
2025-02-05 12:57:24 +0100

    upload

---
+++ main.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class main{
+    public static void main(String[] args) {
+        
+        ArrayList<Vol> Vols = new ArrayList<>();
+
+        Vols.add(new Vol(244, "New York", 1000.00, 210, 89));
+        Vols.add(new Vol(365, "Johannesburg", 1200.25, 180, 50));
+        Vols.add(new Vol(777, "Tokyo", 1500.50,200, 190));
+        Vols.add(new Vol(123, "Rio de Janeiro", 1300.75, 210, 210));
+        Vols.add(new Vol(457, "Istenbul", 900.00, 210, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
